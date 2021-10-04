cbc709a6cc2582bd0e0d4fa41d92ee30abd2cbed
bagusjundan <bagusjundan17@gmail.com>
2021-10-04 12:06:57 +0700

    pp

--- src/main.js
+++ src/main.js
@@ -1,4 +1,27 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp, h } from 'vue'
+import routes from './routes'
+
+const App = {
+    data :() => ({
+        currentRoute: window.location.pathname
+    }),
+    computed: {
+        viewComponent() {
+            const matchingPage = routes[this.currentRoute] || '404'
+            return matchingPage
+        }
+    },
+
+    render() {
+        h(this.viewComponent)
+    },
+
+    created() {
+        window.addEventListener('popstate', () => {
+            this.currentRoute = window.location.pathname
+        })
+    }
+
+}
 
 createApp(App).mount('#app')
---
+++ src/routes.js
@@ -0,0 +1,6 @@
+import Home from './pages/Home.vue'
+import About from './pages/About.vue'
+export default {
+    '/': Home,
+    '/about': About
+}
